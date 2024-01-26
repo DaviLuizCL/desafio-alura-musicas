b1a4b407d841007e1079304aaafe0be7e108f847
davi lima <davirasenga@gmail.com>
2024-01-26 19:04:12 -0300

    Primeiro Commit

---
+++ src/main/java/com/alura/musicas/AluraMusicas/AluraMusicasApplication.java
@@ -0,0 +1,13 @@
+package com.alura.musicas.AluraMusicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraMusicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraMusicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/musicas/AluraMusicas/AluraMusicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.musicas.AluraMusicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraMusicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
